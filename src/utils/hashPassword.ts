8ca09977d1a5f7df5a9010a34164de214f2f10d6
yusufbaskeser <yusufbaskeser2005@gmail.com>
2025-12-17 00:31:06 +0300

    feat(db): Files for database connection (Epic 1.2)

--- src/utils/hashPassword.ts
+++
@@ -1,6 +0,0 @@
-import * as bcrypt from 'bcryptjs';
-
-export async function hashPassword(password: string): Promise<string> {
-  const salt = await bcrypt.genSalt(10);
-  return bcrypt.hash(password, salt);
-}
