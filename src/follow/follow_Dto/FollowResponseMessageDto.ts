07c971da84007a0594a5b64be550c96bd37c40d8
yusufbaskeser <yusufbaskeser2005@gmail.com>
2025-12-29 18:33:47 +0300

    docker added

--- src/follow/follow_Dto/FollowResponseMessageDto.ts
+++
@@ -1,7 +0,0 @@
-export class FollowResponseMessageDto {
-  message: string;
-
-  constructor(message: string) {
-    this.message = message;
-  }
-}
--- src/utils/generateJwtToken.ts
+++
@@ -1,9 +0,0 @@
-import * as jwt from 'jsonwebtoken';
-
-const JWT_SECRET = process.env.JWT_SECRET || 'super-secret-key';
-
-export function generateJwtToken(payload: any): string {
-  return jwt.sign(payload, JWT_SECRET, {
-    expiresIn: '30d',
-  });
-}
